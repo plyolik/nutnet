784ee01440ec58e7a78f50328b9589a46324fc1a
plyolik <p.lyolik@mail.ru>
2021-01-25 16:53:39 +0400

    small refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,8 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'NutNet',
-            template: path.resolve(__dirname, './src/template.html'), // шаблон
-            filename: 'index.html', // название выходного файла
+            template: path.resolve(__dirname, './src/template.html'),
+            filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
     ],
